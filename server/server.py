a8cce258886bb63359f15aafe03df3c5c5ea39b2
yhReady <qmopla1048@gmail.com>
2020-03-10 23:24:36 +0900

    chat_app

--- server/server.py
+++ server/server.py
@@ -34,7 +34,6 @@ def client_communication(person):
     :return: None
     """
     client = person.client
-    addr = person.addr
 
     # get persons name
     name = client.recv(BUFSIZ).decode("utf8")
@@ -42,14 +41,20 @@ def client_communication(person):
     broadcast(msg)
 
     while True:
-        msg = client.recv(BUFSIZ)
-        if msg == bytes("{quit}", "utf8"):
-            broadcast(f"{name} has left the chat...", "")
-            client.send(bytes("{quit}", "utf8"))
-            client.close()
-            persons.remove(person)
-        else:
-            client.send(msg, name)
+        try:
+            msg = client.recv(BUFSIZ)
+            print(f"{name} : ", msg.decode("utf8"))
+            if msg == bytes("{quit}", "utf8"):
+                broadcast(f"{name} has left the chat...", "")
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                persons.remove(person)
+                break
+            else:
+                broadcast(msg, name)
+        except Exception as e:
+            print("[EXCEPTION]", e)
+            break
 
 
 def wait_for_connection():
@@ -67,7 +72,7 @@ def wait_for_connection():
             print(f"[COONECTION] {addr} connected to the server {time.time()}")
             Thread(target=client_communication, args=(person,)).start()
         except Exception as e:
-            print("[FAILURE]",e)
+            print("[EXCEPTION]",e)
             run = False
 
     print("SERVER CRASHED")
